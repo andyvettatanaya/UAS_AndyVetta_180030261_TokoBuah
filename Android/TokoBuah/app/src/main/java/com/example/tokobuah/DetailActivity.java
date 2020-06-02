8d2e3e503e5913ff61c3d0bf4caed6967f7b2d4e
AndyVetta-tech <65754852+andyvettatanaya@users.noreply.github.com>
2020-06-02 17:32:37 +0800

    Update DetailActivity.java

--- Android/TokoBuah/app/src/main/java/com/example/tokobuah/DetailActivity.java
+++ Android/TokoBuah/app/src/main/java/com/example/tokobuah/DetailActivity.java
@@ -3,7 +3,6 @@ package com.example.tokobuah;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
